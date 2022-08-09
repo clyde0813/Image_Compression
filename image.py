04560ea34a6abacc33842c16bd0dc2484eafb7e1
clyde0813 <eric3285@naver.com>
2022-08-09 19:21:44 +0900

    2nd

--- image.py
+++ image.py
@@ -1,18 +1,23 @@
 from io import BytesIO
 from PIL import Image
-import os 
+import os
 
-def compress(image):    
-    im = Image.open(image)   
-    im_io = BytesIO()     
-    im.save(im_io, 'JPEG', quality=60)     
-    with open('Compressed_lena.jpg', 'wb') as file:
+
+def compress(image):
+    im = Image.open('original/'+image)
+    im_io = BytesIO()
+    im.save(im_io, 'JPEG', quality=60)
+    with open('compressed/Compressed_'+image+'.jpg', 'wb') as file:
         file.write(im_io.getvalue())
-    original_image_size = os.path.getsize(image)
-    compressed_image_size = os.path.getsize('Compressed_lena.jpg')
-    print('Original Lena Size : ', original_image_size, '\nCompressed Lena Size : ', compressed_image_size, '\nCompression Percent : ', original_image_size/compressed_image_size * 100, '%')
+    original_image_size = os.path.getsize('original/'+image)
+    compressed_image_size = os.path.getsize('compressed/Compressed_'+image+'.jpg')
+    print('Original Lena Size : ', original_image_size, '\nCompressed Lena Size : ', compressed_image_size,
+          '\nCompression Percent : ', original_image_size / compressed_image_size * 100, '%')
 
-compress('lena.png') 
+
+file_list = os.listdir('original')
+for file in file_list:
+    compress(file)
 
 '''
 https://stackoverflow.com/questions/67629703/compressing-a-video-file-in-python-with-the-standard-library
